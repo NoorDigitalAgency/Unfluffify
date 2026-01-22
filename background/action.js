cac7ff3c7d0e13d7cc50c50acf6b401518bb46a0
Rojan Gh. <sojaner@gmail.com>
2026-01-22 11:24:50 +0100

    Organizes icon files into subdirectories
    Moves icon files into `icons/active` and `icons/default` subdirectories.
    This change improves project structure and maintainability.

--- background/action.js
+++ background/action.js
@@ -8,16 +8,16 @@ export async function updateActionForTab(tabId) {
   const enabled = state && state.enabled;
   const path = enabled
     ? {
-        16: "active/icon16.png",
-        32: "active/icon32.png",
-        48: "active/icon48.png",
-        128: "active/icon128.png"
+        16: "icons/active/icon16.png",
+        32: "icons/active/icon32.png",
+        48: "icons/active/icon48.png",
+        128: "icons/active/icon128.png"
       }
     : {
-        16: "icons/icon16.png",
-        32: "icons/icon32.png",
-        48: "icons/icon48.png",
-        128: "icons/icon128.png"
+        16: "icons/default/icon16.png",
+        32: "icons/default/icon32.png",
+        48: "icons/default/icon48.png",
+        128: "icons/default/icon128.png"
       };
   chrome.action.setIcon({ tabId, path }, () => {
     void chrome.runtime.lastError;
